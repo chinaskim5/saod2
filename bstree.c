93d583dbc13c885d4edbef73f34d881475a63f31
Chinaski <emindanew@yandex.ru>
2017-04-07 19:40:46 +0300

    bstree

---
+++ bstree.c
@@ -0,0 +1,82 @@
+#include "bstree.h"
+
+
+
+bstree *bstree_create(char *key, char *value){
+
+    bstree *node;
+    node= malloc(sizeof(*node));
+    if (node != NULL){
+	node->key=key;
+	node->value=value;
+	node->left=NULL;
+	node->right=NULL;
+    }
+    return node;
+}
+
+bstree *bstree_add(bstree *tree,char *key, char *value){
+    bstree *parent,*node;
+    
+    if (tree == NULL)
+	return;
+    
+    for(parent=tree; tree != NULL; ){
+	parent = tree;
+	if ( key < tree->key)
+	    tree = tree->left;
+	else if (key > tree->key)
+	    tree = tree->right;
+	else
+	    return;
+    }
+    node = bstree_create(key, value);
+    if (key < parent->key)
+	parent->left= node;
+    else
+	parent->right = node;
+
+}
+
+bstree * bstree_lookup(bstree *tree, char *key){
+
+    while (tree !=NULL){
+	if (key == tree->key){
+	    return tree;
+	} else if (*key < *tree->key){
+	    tree= tree->left;
+	} else {
+	    tree= tree->right;
+	}
+    }
+    return tree;
+}
+
+bstree *bstree_max(bstree *tree){
+
+    if (tree == NULL)
+	return NULL;
+	
+    while (tree->right !=NULL)
+	tree = tree->right;
+    return tree;
+}
+
+bstree *bstree_min(bstree *tree){
+
+    if (tree == NULL)
+	return NULL;
+
+    while (tree->left != NULL)
+	tree = tree->left;
+
+    return tree;
+}
+
+
+
+
+
+
+
+
---
+++ bstree.h
@@ -0,0 +1,21 @@
+#ifndef BSTREE_H
+#define BSTREE_H
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct binary_tree{
+    char *key;
+    int value;
+    struct binary_tree *left;
+    struct binary_tree *right;
+} bstree;
+
+
+bstree *bstree_create(char *key, char *value);
+bstree *bstree_add(bstree *tree,char *key, char *value);
+bstree *bstree_lookup(bstree *tree, char *key);
+bstree *bstree_min(bstree *tree);
+bstree *bstree_max(bstree *tree);
+
+
+#endif
